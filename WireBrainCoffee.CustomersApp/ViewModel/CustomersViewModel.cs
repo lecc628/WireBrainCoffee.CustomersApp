8e81c8217c53ca5cbd714e26c602ab563ca6f0dc
lecc628 <lecc628@gmail.com>
2022-05-03 23:16:48 +0200

    Create a CustomersViewModel

---
+++ WireBrainCoffee.CustomersApp/ViewModel/CustomersViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using WireBrainCoffee.CustomersApp.Data;
+using WireBrainCoffee.CustomersApp.Model;
+
+namespace WireBrainCoffee.CustomersApp.ViewModel
+{
+    public class CustomersViewModel
+    {
+        private readonly ICustomerDataProvider _customerDataProvider;
+
+        public CustomersViewModel(ICustomerDataProvider customerDataProvider)
+        {
+            _customerDataProvider = customerDataProvider;
+        }
+
+        public ObservableCollection<Customer> Customers { get; } = new();
+
+        public async Task LoadAsync()
+        {
+            if (Customers.Any())
+            {
+                return;
+            }
+
+            var customers = await _customerDataProvider.GetAllAsync();
+
+            if (customers is not null)
+            {
+                foreach (var customer in customers)
+                {
+                    Customers.Add(customer);
+                }
+            }
+
+        }
+    }
+}
