677a4aa4e30562ffa8504fdabeaa4f41046763f6
lecc628 <lecc628@gmail.com>
2022-05-10 13:42:29 +0200

    Set up dependency injection

--- WireBrainCoffee.CustomersApp/App.xaml.cs
+++ WireBrainCoffee.CustomersApp/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
 using WireBrainCoffee.CustomersApp.Data;
 using WireBrainCoffee.CustomersApp.ViewModels;
 
@@ -9,15 +10,34 @@ namespace WireBrainCoffee.CustomersApp
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            _serviceProvider = ConfigureServices();
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            var mainWindow = new MainWindow(new MainViewModel(
-                new CustomersViewModel(new CustomerDataProvider()),
-                new ProductsViewModel()
-            ));
-            mainWindow.Show();
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
+        }
+
+        private static ServiceProvider ConfigureServices()
+        {
+            ServiceCollection services = new();
+
+            services.AddTransient<MainWindow>();
+
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<CustomersViewModel>();
+            services.AddTransient<ProductsViewModel>();
+
+            services.AddTransient<ICustomerDataProvider, CustomerDataProvider>();
+
+            return services.BuildServiceProvider();
         }
     }
 }
