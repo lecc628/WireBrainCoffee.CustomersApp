928a88583f9d2c99ae8434a41b01a529adbb1a30
lecc628 <lecc628@gmail.com>
2022-05-14 07:05:29 +0200

    Create a ValidationViewModelBase class

---
+++ WireBrainCoffee.CustomersApp/ViewModels/ValidationViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+
+namespace WireBrainCoffee.CustomersApp.ViewModels
+{
+    public class ValidationViewModelBase : ViewModelBase, INotifyDataErrorInfo
+    {
+        private readonly Dictionary<string, List<string>> _errorsByPropertyName = new();
+
+        public bool HasErrors => _errorsByPropertyName.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public IEnumerable GetErrors(string? propertyName) => ((propertyName is not null) && (_errorsByPropertyName.ContainsKey(propertyName)))
+            ? _errorsByPropertyName[propertyName]
+            : Enumerable.Empty<string>();
+
+        protected virtual void OnErrorsChanged(DataErrorsChangedEventArgs e) => ErrorsChanged?.Invoke(this, e);
+    }
+}
